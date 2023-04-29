ab6b527e9935f3669c5da87d23f6369b8a75970b
fm39hz <hitpoint2k3@gmail.com>
2023-04-29 22:35:45 +0700

    new file:   .gitattributes 	new file:   .gitignore 	new file:   .vscode/ProjectA.code-workspace 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   Asset/Texture/Doctor_placeholder.png 	new file:   Asset/Texture/Doctor_placeholder.png.import 	new file:   ProjectA.csproj 	new file:   ProjectA.sln 	new file:   README.MD 	new file:   Scene/Moveable/Player.tscn 	new file:   Scene/Static/static_body_2d.tscn 	new file:   Scene/root_scene.tscn 	new file:   Script/Player.cs 	new file:   export_presets.cfg 	new file:   icon.svg 	new file:   icon.svg.import 	new file:   project.godot

---
+++ Script/Player.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+public partial class Player : CharacterBody2D{
+	[Export]
+		float Speed = 0.0f;
+		private Vector2 GetMovementInput(Vector2 input){
+			Vector2 _targetInput;
+			float _horizontal = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+			float _vertival = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+				_targetInput = new Vector2(_horizontal, _vertival);
+			return _targetInput.Normalized();
+			}
+		public override void _PhysicsProcess(double delta){
+			Velocity = GetMovementInput(Velocity) * Speed;
+			MoveAndSlide();
+			}
+	}
